389266216adec144a9ae21c7f4c765990d2e2cf5
Connor Brewster <cbrewster@hey.com>
2023-03-16 12:24:40 -0500

    Remove input digest debug log

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
-use std::io::{self, Cursor, Read, Write};
-
-use sha2::{Digest, Sha256};
+use std::io::{self, Cursor};
 
 mod encode;
 mod zinfo;
@@ -10,34 +8,9 @@ mod ztoc;
 #[path = "../target/flatbuffers/ztoc_generated.rs"]
 pub mod ztoc_flatbuffers;
 
-struct Tee<R, W> {
-    reader: R,
-    writer: W,
-}
-
-impl<R, W> Tee<R, W> {
-    fn new(reader: R, writer: W) -> Self {
-        Self { reader, writer }
-    }
-}
-
-impl<R, W> Read for Tee<R, W>
-where
-    R: Read,
-    W: Write,
-{
-    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
-        let read = self.reader.read(buf)?;
-        self.writer.write_all(&buf[..read])?;
-        Ok(read)
-    }
-}
-
 fn main() -> io::Result<()> {
-    let mut hasher = Sha256::new();
-    let ztoc = ztoc::ZToc::new(Tee::new(std::io::stdin(), &mut hasher))?;
+    let ztoc = ztoc::ZToc::new(std::io::stdin())?;
     let encoded = encode::encode_ztoc(&ztoc);
     std::io::copy(&mut Cursor::new(encoded), &mut std::io::stdout())?;
-    eprintln!("Digest: sha256:{:x}", hasher.finalize());
     Ok(())
 }
